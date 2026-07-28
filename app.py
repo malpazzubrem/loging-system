40a742a2066fa533ccabb7b1bf9215f7a251f105
malpazzubrem <karolbandura@proton.me>
2026-07-28 18:29:26 +0200

    adding admin panel and updating wrong password mechanism

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
+from flask_session import Session
+from getpass import getpass
+from flask import redirect
+
+
+#to do
+#session cookies
+#privalages
+#
 
 app = Flask(__name__)
 
@@ -9,14 +18,59 @@ bcrypt = Bcrypt(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #configuring base
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem" #configuring session
+
 db = SQLAlchemy(app)
+Session(app)
 
 class Profile(db.Model): #setting up sql
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20),unique=False,nullable=False)
-    password = db.Column(db.String(20),unique=False,nullable=False)
+    password = db.Column(db.String(128),unique=False,nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
+    def __repr__(self):
+        return f"username : {self.username}"
+
+@app.cli.command("create-admin")
+def create_admin():
+    username = input("write admin username: ")
+    if Profile.query.filter_by(username=username).first():
+        print("That username already exists.")
+        return
+    password = getpass("Password: ")
+    hashed = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = Profile(username=username,password=hashed ,is_admin=True)
+
+    db.session.add(user)
+    db.session.commit()
+    print("admin user created")
+
+@app.cli.command("delete-admin")
+def delete_admin():
+    username = input("write admin username: ")
+    data = data = Profile.query.filter_by(username=username).first()
+    if data==None:
+        print("no admin found")
+        return
     
+    print("are you sure? [y/n]")
+    ans = input()
+
+    if ans == "y":
+        db.session.delete(data)
+        db.session.commit()
+        print("admin removed")
+
+    else:
+        print("aborted")
+
+@app.cli.command("list-admins")
+def list_admins():
+    data = Profile.query.filter_by(is_admin=True)
+    for i in data:
+        print(i.username)
 
 @app.route("/",methods=["GET","POST"])
 def main_page():
@@ -38,13 +92,19 @@ def login():
 
             user_1 = bcrypt.check_password_hash(user.password, password) #checking passwords
 
-            if user_1:
+            if user_1 and user.is_admin:
+                session["user.id"] = user.id
+                session["is_admin"] = user.is_admin
+                print("admin login")
+                return redirect('/admin')
+
+            elif user_1:
                 print('login successful')
                 return render_template("welcome_ou.html")
+            
             else:
                 print('wrong password')
-                return render_template("wrong_password.html")
-
+                return render_template("log_in.html",error="Wrong username or password")
 
     return render_template("log_in.html")
 
@@ -63,6 +123,51 @@ def signin():
             return render_template("welcome_nu.html")
 
     return render_template("sign_in.html")
+
+@app.route("/admin",methods=["GET","POST"])
+def admin_panel():
+    if not("user.id" in session):
+        return redirect("/")
+    profiles = Profile.query.all()
+    print("admin accesing page")
+
+    return render_template('admin_panel.html', profiles=profiles)
+
+@app.route("/logout")
+def loging_out():
+    print("admin loging out")
+    session.pop("user.id",None)
+    return redirect("/")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    if not("user.id" in session):
+        return redirect("/")
+    data = db.session.get(Profile, id)
+    db.session.delete(data)
+    db.session.commit()
+    return redirect("/admin")
+
+@app.route("/edit/<int:id>",methods=["POST","GET"])
+def edit(id):
+    if not("user.id" in session):
+        return redirect("/")
+    data = db.session.get(Profile,id)
+    if request.method=="POST":
+        new_username = request.form["username"]
+        new_password = request.form["password"]
+
+        if not(new_username==""):
+            data.username = new_username
+        if not(new_password==""):
+            hashed = bcrypt.generate_password_hash(new_password).decode('utf-8')
+            data.password = hashed
+        
+        db.session.commit()
+
+        return redirect("/admin")
+
+    return render_template("editing_user.html")
 
 if __name__ == "__main__":
     with app.app_context(): 
